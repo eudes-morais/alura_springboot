7436aa7d39bc9629fd40c8882fbad1745f277dcd
Eudes Morais <eudes.morais@gmail.com>
2025-11-11 18:19:11 -0300

    Dependências e Migrations com Flyway

--- src/main/java/br/med/voll/api/controller/MedicoController.java
+++ src/main/java/br/med/voll/api/controller/MedicoController.java
@@ -1,18 +1,24 @@
 package br.med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.med.voll.api.medico.DadosCadastroMedico;
+import br.med.voll.api.medico.Medico;
+import br.med.voll.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping
+    @org.springframework.transaction.annotation.Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
